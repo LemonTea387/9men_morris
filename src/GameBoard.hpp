e0cad3d85d6b51cc2ed6a300800b3764309e2cee
nrav0005 <nrav0005@student.monash.edu>
2023-04-15 00:13:31 +0800

    Added basic rendering of tokens.

--- src/GameBoard.hpp
+++ src/GameBoard.hpp
@@ -1,13 +1,22 @@
 #ifndef GAMEBOARD_H
 #define GAMEBOARD_H
 #include <SFML/Graphics.hpp>
+#include <unordered_map>
 
 #include "GameObject.hpp"
+#include "SFML/Graphics/CircleShape.hpp"
+#include "Tile.hpp"
 
 class GameBoard : public GameObject {
  private:
+  typedef std::map<Tile::TileCoord, Tile::Tile> Tiles;
+  Tiles m_Tiles;
   sf::RectangleShape m_Board;
   sf::Texture m_BoardTexture;
+
+  sf::CircleShape m_DebugToken;
+
+  void InitialiseTiles();
 
  public:
   void Update(sf::Event event);
---
+++ src/Tile.hpp
@@ -0,0 +1,29 @@
+#ifndef TILE_H
+#define TILE_H
+#include <SFML/Graphics.hpp>
+
+namespace Tile {
+
+/**
+ * TO-DO: Create class for TileCoord with proper bounds checkings.
+ * Can  also possibly overload the hash function () operator / equality check
+ * to use std::unordered_map for a tiny bit extra performance.
+ */
+typedef std::pair<int, int> TileCoord;
+
+enum class Occupation { DOGE, PEPE, NONE };
+
+struct Tile {
+  /**
+   * Position withing screen space.
+   */
+  sf::Vector2f coords;
+  /**
+   * Position within the "Game Board"
+   */
+  sf::Vector2i position;
+  Occupation occupation;
+};
+
+}  // namespace Tile
+#endif
