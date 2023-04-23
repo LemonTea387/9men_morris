558edc174dd2fa1a96a7c4dd2c3994ce4db99768
jlow0030 <jlow0030@student.monash.edu>
2023-04-23 22:41:36 +0800

    Scenes store a collection of UI's for easy Event propagation.

--- src/Scene/Scene.hpp
+++ src/Scene/Scene.hpp
@@ -1,10 +1,15 @@
 #ifndef SCENE_H
 #define SCENE_H
 #include "../GameObject.hpp"
+#include "ui/UI.hpp"
 class Scene : public GameObject {
  public:
   virtual void Update(sf::Event event) = 0;
   virtual void Render() = 0;
+  void addUI(graphics::UI* ui) { m_ui.emplace_back(ui); }
+
+ protected:
+  std::vector<graphics::UI*> m_ui{};
 };
 
 #endif
