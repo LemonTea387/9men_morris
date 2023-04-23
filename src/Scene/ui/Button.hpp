5b08d233ec7b418e3c3dee723343455ee9cf34fb
jlow0030 <jlow0030@student.monash.edu>
2023-04-23 22:40:59 +0800

    Incorporated Button to use OnClickEventListener.

--- src/Scene/ui/Button.hpp
+++ src/Scene/ui/Button.hpp
@@ -2,7 +2,11 @@
 #define BUTTON_H
 
 #include <SFML/Graphics.hpp>
+#include <functional>
 #include <string>
+
+#include "OnClickEventListener.hpp"
+#include "UI.hpp"
 
 /**
  * Button class as an abstraction of a compound UI with Rectangular button and
@@ -10,19 +14,23 @@
  * draw on this compound UI. Position of the Button will be a base position of
  * the compound UI components.
  */
-class Button : public sf::Drawable, public sf::Transformable {
+namespace graphics {
+class Button : public UI {
  public:
   Button(Button const&) = delete;
-  Button();
-  Button(const sf::Texture& texture, const sf::Font& font,
-         const std::string& buttonText);
+  Button(const std::string& buttonText,
+         std::function<void(sf::Event)> onAction);
   ~Button();
+  void setTexture(const sf::Texture& texture);
+  void setFont(const sf::Font& font);
+  void setText(const std::string& text);
 
   /**
    * Setting all the position of the compound UI components to ensure everything
    * looks right.
    */
   void setPosition(const sf::Vector2f& position);
+  virtual void setSize(const sf::Vector2f& size) override;
 
  private:
   static constexpr float MARGIN_X = 32.f;
@@ -31,7 +39,9 @@ class Button : public sf::Drawable, public sf::Transformable {
   sf::RectangleShape m_ButtonShape;
   sf::Text m_ButtonText;
   sf::Font m_ButtonFont;
-  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+  OnClickEventListener m_clickListener;
+  virtual void draw(sf::RenderTarget& target,
+                    sf::RenderStates states) const override;
 };
-
+}  // namespace graphics
 #endif
