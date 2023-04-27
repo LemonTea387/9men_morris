0d0ba9fa1ddcc01cd8c7fe6ace008192420ee3e9
jlow0030 <jlow0030@student.monash.edu>
2023-04-27 17:37:11 +0800

    Workaround for Buttons to calculate string size correctly. (Bug for when font set is not in local scope bounding rect of text is not calculated correctly.)

--- src/Scene/ui/Button.cpp
+++ src/Scene/ui/Button.cpp
@@ -8,17 +8,22 @@ Button::Button(const std::string& buttonText) {
   m_ButtonText.setString(buttonText);
   m_ButtonText.setCharacterSize(CHAR_SIZE);
   m_ButtonText.setFillColor(sf::Color(0xd0, 0xbc, 0xff));
+
+  // TODO : Remove this expensive workaround to fix SFML width = 0 bug with AssetManager
+  if (!m_ButtonFont.loadFromFile(
+          "assets/fonts/Comfortaa/static/Comfortaa-SemiBold.ttf")) {
+    std::cerr << "Could not load ButtonFont" << std::endl;
+  }
+  m_ButtonText.setFont(m_ButtonFont);
+
+  setSize(
+      sf::Vector2f(m_ButtonText.getLocalBounds().width + Button::MARGIN_X * 2,
+                   CHAR_SIZE + Button::MARGIN_Y * 2));
 }
 Button::Button(const std::string& buttonText,
                std::function<void(sf::Event)> onAction)
     : Button(buttonText) {
   m_clickListener = std::make_unique<OnClickEventListener>(this, onAction);
-  // TODO : Calculate dynamically
-  m_ButtonShape.setSize(
-      sf::Vector2f((buttonText.length() - 1) * CHAR_SIZE + Button::MARGIN_X * 2,
-                   CHAR_SIZE + Button::MARGIN_Y * 2));
-  setSize(m_ButtonShape.getSize());
-
   addEventListener(m_clickListener.get());
 }
 
@@ -43,8 +48,8 @@ void Button::setPosition(const sf::Vector2f& position) {
   // ensure getPosition returns the right thing.
   sf::Transformable::setPosition(position);
   m_ButtonShape.setPosition(position);
-  m_ButtonText.setPosition(sf::Vector2f(position.x + Button::MARGIN_X + 7,
-                                        position.y + Button::MARGIN_Y - 5));
+  m_ButtonText.setPosition(sf::Vector2f(position.x + Button::MARGIN_X,
+                                        position.y + Button::MARGIN_Y));
 }
 
 void Button::setSize(const sf::Vector2f& size) {
