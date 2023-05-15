0a2fd44743d10f5faf930493dff662b6cc07722e
jlow0030 <jlow0030@student.monash.edu>
2023-05-16 07:13:29 +0800

    Command allows accessing affected tiles due to observer requiring.

--- src/Command/Command.hpp
+++ src/Command/Command.hpp
@@ -1,9 +1,16 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include "../Tile.hpp"
 class Command {
  public:
+  typedef Tile* TilePtr;
+  Command(TilePtr tile) : m_AffectedTile{tile} {};
+  TilePtr GetAffectedTile() { return m_AffectedTile; };
   virtual void execute() = 0;
+
+ protected:
+  TilePtr m_AffectedTile{nullptr};
 };
 
 #endif
