6d960313f8e82efc9702740deddb41c7940c04d5
jlow0030 <jlow0030@student.monash.edu>
2023-05-18 13:22:00 +0800

    Brief docs on observer

--- src/Observer/Observer.hpp
+++ src/Observer/Observer.hpp
@@ -3,13 +3,34 @@
 
 #include "../Tile.hpp"
 
+/**
+ * Observes the GameBoard for any changes from a Command that may lead to a new
+ * gameplay state or progression. This is used for detecting any game rule such
+ * as Mill formation in the GameBoard. Needs to be Notify() after every Command
+ * from GameBoard.
+ */
 class Observer {
  public:
+  /**
+   * Observer Constructor, the gameboard here refers to the gameboard the
+   * Observer is observing on.
+   */
   Observer(GameBoard* gameboard) : m_Gameboard{gameboard} {};
+
+  /**
+   * Observer Destructor.
+   */
   ~Observer() = default;
+
+  /**
+   * Used to notify the observer with the Tile that's affected.
+   */
   virtual void Notify(Tile* tileAffected) = 0;
 
  protected:
+  /**
+   * GameBoard tha the observer observes.
+   */
   GameBoard* m_Gameboard;
 };
 #endif
