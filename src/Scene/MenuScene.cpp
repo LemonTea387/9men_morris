a53d8e3591734e48b7b7e97736c10e9267405d96
nrav0005 <nrav0005@student.monash.edu>
2023-05-18 11:18:38 +0800

    MenuScene now uses Buttons and UI elements

--- src/Scene/MenuScene.cpp
+++ src/Scene/MenuScene.cpp
@@ -2,93 +2,44 @@
 
 #include <iostream>
 
+#include "../AssetManager.hpp"
 #include "../Game.hpp"
+#include "GameScene.hpp"
 #include "SFML/Graphics/RenderWindow.hpp"
 #include "SFML/System/Vector2.hpp"
-void MenuScene::Update(sf::Event event) {
-  if (event.type == sf::Event::MouseButtonReleased &&
-      event.mouseButton.button == sf::Mouse::Left) {
-    if (m_ExitButton.getGlobalBounds().contains(event.mouseButton.x,
-                                                event.mouseButton.y)) {
-      Game::GetInstance().PopScene();
-    }
-  }
-}
+#include "SFML/Window/Event.hpp"
+void MenuScene::Update(sf::Event event) { Scene::Update(event); }
 
-void MenuScene::Render(sf::RenderWindow& window) {
-  window.draw(m_NewGameButton);
-  window.draw(m_ConGameButton);
-  window.draw(m_CreditsButton);
-  window.draw(m_ExitButton);
-
-  window.draw(m_NewGameText);
-  window.draw(m_ConGameText);
-  window.draw(m_CreditsText);
-  window.draw(m_ExitText);
-
-  window.draw(m_DogeVsPepe);
-}
+void MenuScene::Render(sf::RenderWindow& window) { Scene::Render(window); }
 
 MenuScene::MenuScene()
-    : m_NewGameButton(sf::Vector2f(261.f, 66.f)),
-      m_ConGameButton(sf::Vector2f(356.f, 74.f)),
-      m_CreditsButton(sf::Vector2f(206.f, 74.f)),
-      m_ExitButton(sf::Vector2f(191.f, 73.f)),
-      m_DogeVsPepe(sf::Vector2f(235.f, 115.f)) {
-  m_NewGameButton.setPosition(319.f, 303.f);
-  m_ConGameButton.setPosition(272.f, 395.f);
-  m_CreditsButton.setPosition(347.f, 495.f);
-  m_ExitButton.setPosition(354.f, 595.f);
-  m_DogeVsPepe.setPosition(602.f, 728.f);
+    : m_DogeVsPepe{sf::Vector2f(235.f, 115.f)},
+      m_NewGameButton{
+          "New Game",
+          [&](sf::Event e) {
+            Game::GetInstance().PushScene(std::make_unique<GameScene>());
+          }},
+      m_CreditsButton{"Credits", [&](sf::Event e) {}},
+      m_ExitButton{"Quit",
+                   [&](sf::Event e) { Game::GetInstance().PopScene(); }} {
+  AssetManager& assMan = AssetManager::GetInstance();
+  m_NewGameButton.setPosition(sf::Vector2f(319.f, 303.f));
+  m_NewGameButton.setTexture(
+      assMan.GetTexture(GameAsset::BUTTON_NEWGAME).get());
+  m_CreditsButton.setPosition(sf::Vector2f(342.f, 415.f));
+  m_CreditsButton.setTexture(assMan.GetTexture(GameAsset::BUTTON).get());
+  m_ExitButton.setPosition(sf::Vector2f(367.f, 535.f));
+  m_ExitButton.setTexture(assMan.GetTexture(GameAsset::BUTTON).get());
+  m_DogeVsPepe.setPosition(sf::Vector2f(602.f, 728.f));
 
-  m_NewGameText.setPosition(348.5f, 316.f);
-  m_ConGameText.setPosition(312.5f, 405.f);
-  m_CreditsText.setPosition(387.5f, 505.f);
-  m_ExitText.setPosition(409.f, 610.f);
-  if (!m_ButtonFont.loadFromFile(
-          "assets/fonts/Comfortaa/static/Comfortaa-SemiBold.ttf")) {
-    std::cerr << "Could not load font!" << std::endl;
-  }
-  if (!m_NewGameTexture.loadFromFile("assets/ui/buttons/NewGame.png")) {
-    std::cerr << "Could not load UI button!" << std::endl;
-  }
-  if (!m_ConGameTexture.loadFromFile("assets/ui/buttons/ConGame.png")) {
-    std::cerr << "Could not load UI button!" << std::endl;
-  }
-  if (!m_CreditsTexture.loadFromFile("assets/ui/buttons/Credits.png")) {
-    std::cerr << "Could not load UI button!" << std::endl;
-  }
-  if (!m_ExitTexture.loadFromFile("assets/ui/buttons/Exit.png")) {
-    std::cerr << "Could not load UI button!" << std::endl;
-  }
+  addUI(&m_NewGameButton);
+  addUI(&m_CreditsButton);
+  addUI(&m_ExitButton);
+
   if (!m_DogeVsPepeTexture.loadFromFile("assets/ui/buttons/DogeVsPepe.png")) {
     std::cerr << "Could not load DogeVsPepe!" << std::endl;
   }
-  std::cerr << "Could not load DogeVsPepe!" << std::endl;
-  m_NewGameButton.setTexture(&m_NewGameTexture);
-  m_ConGameButton.setTexture(&m_ConGameTexture);
-  m_CreditsButton.setTexture(&m_CreditsTexture);
-  m_ExitButton.setTexture(&m_ExitTexture);
   m_DogeVsPepe.setTexture(&m_DogeVsPepeTexture);
-
-  m_NewGameText.setFont(m_ButtonFont);
-  m_NewGameText.setString("New Game");
-  m_NewGameText.setCharacterSize(32);
-  m_NewGameText.setFillColor(sf::Color(0xd0, 0xbc, 0xff));
-
-  m_ConGameText.setFont(m_ButtonFont);
-  m_ConGameText.setString("Continue Game");
-  m_ConGameText.setCharacterSize(32);
-  m_ConGameText.setFillColor(sf::Color(0xd0, 0xbc, 0xff));
-
-  m_CreditsText.setFont(m_ButtonFont);
-  m_CreditsText.setString("Credits");
-  m_CreditsText.setCharacterSize(32);
-  m_CreditsText.setFillColor(sf::Color(0xd0, 0xbc, 0xff));
-
-  m_ExitText.setFont(m_ButtonFont);
-  m_ExitText.setString("Quit");
-  m_ExitText.setCharacterSize(32);
-  m_ExitText.setFillColor(sf::Color(0xd0, 0xbc, 0xff));
+  addDrawable(&m_DogeVsPepe);
 }
 MenuScene::~MenuScene() {}
--- src/Scene/MenuScene.hpp
+++ src/Scene/MenuScene.hpp
@@ -5,26 +5,17 @@
 
 #include "SFML/Graphics/RectangleShape.hpp"
 #include "Scene.hpp"
+#include "ui/Button.hpp"
 
 class MenuScene : public Scene {
  private:
   // TO-DO: Use a Button class (with rounded corners, hover support, etc.)
-  sf::RectangleShape m_NewGameButton;
-  sf::RectangleShape m_ConGameButton;
-  sf::RectangleShape m_CreditsButton;
-  sf::RectangleShape m_ExitButton;
   sf::RectangleShape m_DogeVsPepe;
-  sf::Text m_NewGameText;
-  sf::Text m_ConGameText;
-  sf::Text m_CreditsText;
-  sf::Text m_ExitText;
-  sf::Texture m_NewGameTexture;
-  sf::Texture m_ConGameTexture;
-  sf::Texture m_CreditsTexture;
-  sf::Texture m_ExitTexture;
   sf::Texture m_DogeVsPepeTexture;
 
-  sf::Font m_ButtonFont;
+  graphics::Button m_NewGameButton;
+  graphics::Button m_CreditsButton;
+  graphics::Button m_ExitButton;
 
  public:
   MenuScene();
