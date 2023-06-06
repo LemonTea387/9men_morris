b72443d8819f7bd186cbec9dc623e6d25f3ccec4
jia zheng <jcha0170@student.monash.edu>
2023-06-06 19:51:57 +0800

    move methods in textfield to public

--- src/Scene/ui/TextField.hpp
+++ src/Scene/ui/TextField.hpp
@@ -10,16 +10,18 @@ class TextField : public sf::Transformable, public sf::Drawable {
   TextField();
   ~TextField();
   void handleInput(sf::Event e);
+  void Update(sf::Event e);
+  bool contains(sf::Vector2f point) const;
+  void setFocus(bool focus);
+  const std::string& getText() const;
+  void setPosition(float x, float y);
+  virtual void draw(sf::RenderTarget& target,
+                    sf::RenderStates states) const override;
 
  private:
   unsigned int m_size;
   std::string m_text;
   sf::RectangleShape m_rect;
   bool m_hasfocus;
-  const std::string& getText() const;
-  void setPosition(float x, float y);
-  bool contains(sf::Vector2f point) const;
-  void setFocus(bool focus);
-  virtual void draw(sf::RenderTarget& target,
-                    sf::RenderStates states) const override;
+  
 };
