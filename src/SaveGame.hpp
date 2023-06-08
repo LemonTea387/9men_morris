81defc57dc1d7e4dc874cb3f70b3a23a3aab787e
jia zheng <jcha0170@student.monash.edu>
2023-06-08 16:52:06 +0800

    document GameSave

--- src/SaveGame.hpp
+++ src/SaveGame.hpp
@@ -14,19 +14,57 @@ class SaveGame {
  private:
   typedef std::unique_ptr<Command> CommandPtr;
   typedef std::vector<CommandPtr> CommandPtrStack;
+
+  /*
+  * To store the commands previously called.
+  */
   CommandPtrStack* m_Commands;
+
+  /**
+   * Pointer to the GameBoard the GameSave belongs to for communicating 
+   * with the GameBoard.
+   */
   GameBoard* m_GameBoard;
+
+  /*
+  * A string as buffer for file content.
+  */
   std::vector<std::string> m_FileContents;
 
+  /*
+  * This deserialize a string, create it's corresponding command, the
+  * add it to stack.
+  */
   void AddCommandFromString(const std::string& line);
 
  public:
+  /*
+  * This function aappends a serializedstring into accumulated seralized 
+  * strings.
+  */
   void AddToSave(const std::string& serialised);
+
+  /*
+  * This function loads a previous saved game state from a text file 
+  * with name 'filename'.
+  */
   void LoadFromSave(const std::string& filename);
+
+  /*
+  * This function save current game state to a text file with name 
+  * 'filename'.
+  */
   void SaveGameFile(const std::string& filename);
 
-  std::vector<std::string> GetFileContents() const { return m_FileContents; }
+  /*
+  * SaveGame constructor, it uses the gameboard pointer to communicate with the
+   * gameboard directly.
+  */
   SaveGame(GameBoard* gb);
+
+  /*
+  * SaveGame destructor.
+  */
   ~SaveGame();
 };
 
